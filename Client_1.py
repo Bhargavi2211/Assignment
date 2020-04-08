2fa59b35a5a4430946bfafbc99aa7d30573497f1
Bhargavi <62302058+Bhargavi2211@users.noreply.github.com>
2020-04-09 02:23:06 +0530

    Create Client_1.py

---
+++ Client_1.py
@@ -0,0 +1,21 @@
+import socket
+import sys
+import time
+
+s = socket.socket()
+host = input(str("Please enter hostname of server:"))
+port = 2606
+
+s.connect((host, port))
+
+print("Connect to chat server")
+while True:
+    incoming_message = s.recv(1024)
+    incoming_message = incoming_message.decode()
+    print("Server:", incoming_message)
+    print("")
+    message = input(str(">> "))
+    message = message.encode()
+    s.send(message)
+    print("Message has been sent")
+    print("")
