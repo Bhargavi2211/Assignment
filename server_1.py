58134f95f8c4a9b025836954a467ddb70abf8ca4
Bhargavi <62302058+Bhargavi2211@users.noreply.github.com>
2020-04-09 13:35:47 +0530

    Update server_1.py

--- server_1.py
+++ server_1.py
@@ -8,15 +8,15 @@ s.bind((host, port))
 print("Server done binding to host and port successfully")
 print("Server is waiting for connection")
 s.listen(1)
-conn, addr = s.accept()
+connection, addr = s.accept()
 print(addr, "Has connected to server and online")
 
 while True:
     message = input(str(""))
     message = message.encode()
-    conn.send(message)
+    connection.send(message)
     print("Message has been sent")
-    incoming_message = conn.recv(1024)
+    incoming_message = connection.recv(1024)
     incoming_message = incoming_message.decode()
     print("Client:", incoming_message)
    
